bb41191e74a419f0ba619a3e6c5254decb867019
Alan Kuzmanovic <alankuzmanovic@gmail.com>
2019-08-12 16:30:10 -0600

    Added readme information/labeled functions

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # GiphyInAJiffy
+
+This project uses the GIPHY API to display paused .gifs from their website, once clicked the .gifs animate. It includes the ratings, as well as the titles of each of the random, user uploaded .gifs.
+
+## Getting Started
+
+Simply press a country button in the list provide on page load. If you would like to add a new country, then type it into the seach bar and press the submit button. If you would like the .gifs to animate, simply click them to have them play and click again to pause them. 
+
+### Prerequisites
+
+-Fingers
+-Functional mouse/trackpad
+-Newest browser update recommended**
+
+### Installing
+
+To install, and modify this program, you may press the fork button and then fork it to your computer through either gitbash, or terminal. To test the API you may press the following link:
+[Giphy In A Jiffy](https://alankuz.github.io/GiphyInAJiffy/)
+
+## Deployment
+
+You may fork the files, or use the link to add the program to your own pages.
+
+## Built With
+
+* [Jquery](https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js) - Script framework
+* [Giphy API](https://developers.giphy.com/) - .gif API
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/alankuz/GiphyInAJiffy/tags). 
+
+## Author
+
+* **Alan Kuzmanovic** - *Initial work* - [alankuz](https://github.com/alankuz)
+
+## License
+
+No license agreements, do as you will with the program. 
+
+## Psuedocode:
+
+On load> Create 10 buttons labeled under a different country
+
+On button press> Take label name, place information into the Giphy link, load 2 seperate version of the gif, 1 animated, 1 still>Put the .gifs into an assigned DIV
+
+On search input>Put new name/country into the Button div created earlier, by appending> Make new button follow original on click handler to generate image
+
+On image click>switch the original link of the still .gif>On click again>switch to animated .gif
+
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// Global Variables/API key--------------------
 var apiKey = 'e6CTlgKsAG73BWRQ44ILXE39GgZSQAFV'
+
 var topics = [
     "Bosnia",
     "Croatia",
@@ -12,57 +14,50 @@ var topics = [
     "Ireland",
     "Scotland",
     "Britain"
-
-
 ]
-for (i=0; i<topics.length; i++) {
-    $('#buttons').append('<button class= "contentButton" data-place='+topics[i]+ '>'+ topics[i]+'</button>')
+
+// On load buttons-----------------
+for (i = 0; i < topics.length; i++) {
+    $('#buttons').append('<button class= "contentButton" data-place=' + topics[i] + '>' + topics[i] + '</button>')
 }
+
+// Pause function---------------------
 function pauser(i) {
-    console.log('click')
-    console.log(i)
-    
 
-var state = $('#'+i).attr('data-state');
-console.log(state);
-
-if (state === "still") {
-$('#'+i).attr('src', $('#'+i).attr("data-animate"))
-$('#'+i)
-$('#'+i).attr('data-state', 'animated')
-}
-if (state === "animated") {
-$('#'+i).attr('src', $('#'+i).attr("data-still"))
-$('#'+i)
-$('#'+i).attr('data-state', 'still')
-}
-}
-
-// var url ="https://api.giphy.com/v1/gifs/search?api_key=0bLXaO5E9THqBOtzSMn61Yk0RFnSYcq1&q=" +topics[2] +"&limit=25&offset=0&rating=pg&lang=en"
-
-// startDate + endDate  ""
-// https://api.nytimes.com/svc/search/v2/articlesearch.json
-// Example call https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
-// q=obama&facet_fields=source&facet=true&begin_date=20120101&end_date=20121231
-$("#searchSubmit").click(function (){
-    var newEntry =$('input').val()
-    $('#buttons').append('<button class= "contentButton" data-place='+$('input').val()+ '>'+ $('input').val() +'</button>')
-    })
-
-$(document).on('click', '.contentButton', function(){
-        var country = $(this).attr("data-place");
-        var url = "https://api.giphy.com/v1/gifs/search?api_key=0bLXaO5E9THqBOtzSMn61Yk0RFnSYcq1&q=" +
-          country + "&limit=25&offset=0&rating=pg&lang=en";
-          $('#maindiv').html('');
-$.ajax({
-    type: "GET",
-    url: url,
-    success: function (response) {
-        console.log(response);
-        console.log(response.data[1].images.fixed_height.url)
-        for (i=0; i<25; i++)
-        $('#maindiv').append('<div class="imgcontainer"><p>' + response.data[i].title +'</p> <p>Rating:' + response.data[i].rating + '</p> <img src="' + response.data[i].images.fixed_height_still.url + '" data-still="'+ response.data[i].images.fixed_height_still.url+'" data-animate="'+ response.data[i].images.fixed_height.url+'" data-state="still" class="gif" onclick="pauser('+i+');" id="'+i+'"></div>');        
+    var state = $('#' + i).attr('data-state');
+    if (state === "still") {
+        $('#' + i).attr('src', $('#' + i).attr("data-animate"))
+        $('#' + i)
+        $('#' + i).attr('data-state', 'animated')
     }
-});
+    if (state === "animated") {
+        $('#' + i).attr('src', $('#' + i).attr("data-still"))
+        $('#' + i)
+        $('#' + i).attr('data-state', 'still')
+    }
+}
+
+// Search function--------------------------
+$("#searchSubmit").click(function () {
+    var newEntry = $('input').val()
+    $('#buttons').append('<button class= "contentButton" data-place=' + $('input').val() + '>' + $('input').val() + '</button>')
+})
+
+// Button click function(displays the .gifs for the selected country. Loads 25 .gifs from Giphy API)-----------------
+$(document).on('click', '.contentButton', function () {
+
+    var country = $(this).attr("data-place");
+    var url = "https://api.giphy.com/v1/gifs/search?api_key=0bLXaO5E9THqBOtzSMn61Yk0RFnSYcq1&q=" +
+        country + "&limit=25&offset=0&rating=pg&lang=en";
+    $('#maindiv').html('');
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (response) {
+            for (i = 0; i < 25; i++)
+                $('#maindiv').append('<div class="imgcontainer"><p>' + response.data[i].title + '</p> <p>Rating:' + response.data[i].rating + '</p> <img src="' + response.data[i].images.fixed_height_still.url + '" data-still="' + response.data[i].images.fixed_height_still.url + '" data-animate="' + response.data[i].images.fixed_height.url + '" data-state="still" class="gif" onclick="pauser(' + i + ');" id="' + i + '"></div>');
+        }
+    });
 })
 
